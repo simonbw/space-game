0d0e379097a87a4b2280ccd776e7db86cffb2146
simonbw <thecheeseinator@gmail.com>
2014-10-24 19:36:55 -0700

    Added git ignore

--- src/ship/LaserCannon.hx
+++ src/ship/LaserCannon.hx
@@ -8,7 +8,7 @@ import projectiles.Laser;
 
 class LaserCannon extends RectangularPart implements Weapon {
 	
-	static inline var INTERVAL = 0.05;
+	static inline var INTERVAL = 0.15;
 	static inline var ENERGY_USE = 8.0;
 	
 	var cooldown:Float;
